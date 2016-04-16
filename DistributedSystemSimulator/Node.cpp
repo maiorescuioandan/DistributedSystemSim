91ee96c52495f3fa1fd56084cef3a819c1c9d004
Ioan-Dan Maiorescu <ion.maiorescu@gmail.com>
2016-04-16 22:21:41 +0300

    Fixed build error

--- DistributedSystemSimulator/Node.cpp
+++ DistributedSystemSimulator/Node.cpp
@@ -213,15 +213,15 @@ void CNode::CreateLogFile()
 	//	);
 	//boost::log::add_common_attributes();
 	// Construct the sink
-	typedef sinks::synchronous_sink< sinks::text_ostream_backend > text_sink;
-	boost::shared_ptr< text_sink > sink = boost::make_shared< text_sink >();
-
-	// Add a stream to write log to
-	sink->locked_backend()->add_stream(
-		boost::make_shared< std::ofstream >("sample.log"));
-
-	// Register the sink in the logging core
-	logging::core::get()->add_sink(sink);
+	//typedef sinks::synchronous_sink< sinks::text_ostream_backend > text_sink;
+	//boost::shared_ptr< text_sink > sink = boost::make_shared< text_sink >();
+	//
+	//// Add a stream to write log to
+	//sink->locked_backend()->add_stream(
+	//	boost::make_shared< std::ofstream >("sample.log"));
+	//
+	//// Register the sink in the logging core
+	//logging::core::get()->add_sink(sink);
 }
 
 void CNode::TestLogFile()
